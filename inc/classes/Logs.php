9504ad69013898ec895ba5d0aff246ffec90b6fd
RaiN3772 <56936836+RaiN3772@users.noreply.github.com>
2023-04-22 10:58:49 +0300

    Update Logs.php

--- inc/classes/Logs.php
+++ inc/classes/Logs.php
@@ -10,7 +10,7 @@ class Logs extends User {
         return $this->query("SELECT DISTINCT users.name FROM users INNER JOIN user_roles ON users.id = user_roles.user_id INNER JOIN role_permissions ON user_roles.role_id = role_permissions.role_id WHERE role_permissions.permission_id IN (SELECT permission_id FROM role_permissions)")->fetchAll();
     }
     public function getLogs() {
-        return $this->query("SELECT logs.*, users.id, users.name, users.avatar, roles.role_title FROM logs INNER JOIN users ON users.id = logs.user_id INNER JOIN user_roles ON user_roles.user_id = users.id INNER JOIN roles ON roles.role_id = user_roles.role_id")->fetchAll();
+        return $this->query("SELECT logs.*, users.id, users.name, users.avatar, roles.role_title FROM logs INNER JOIN users ON users.id = logs.user_id INNER JOIN user_roles ON user_roles.user_id = users.id INNER JOIN roles ON roles.role_id = user_roles.role_id ORDER BY logs.date DESC")->fetchAll();
     }
     public function getRoles() {
         return $this->query("SELECT DISTINCT roles.role_title FROM roles INNER JOIN role_permissions ON roles.role_id = role_permissions.role_id")->fetchAll();
