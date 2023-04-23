c181277be6da594bea4195afc56ebf8e3834d098
RaiN3772 <56936836+RaiN3772@users.noreply.github.com>
2023-04-24 00:41:40 +0300

    Update profileDetails.php

--- inc/init/post/profileDetails.php
+++ inc/init/post/profileDetails.php
@@ -5,25 +5,25 @@ if (!isset($_POST['confirm_password']) || empty(secure($_POST['confirm_password'
 if (!password_verify($_POST['confirm_password'], $user->hashed())) toastr('error', 'Incorrect password');
 
 if (isset($_POST['display_name']) && !empty(secure($_POST['display_name'])) && $_POST['display_name'] != $user->name()) {
-    if ($profile->allow_name_change()) toastr('error', 'Name change is not allowed');
+    if (!$profile->allow_name_change()) toastr('error', 'Name change is not allowed');
     if (strlen($_POST['display_name']) < $setting->get('minimum_username_length')) toastr('error', 'Name must be at least ' . $setting->get('minimum_username_length') . ' characters');
     if (strlen($_POST['display_name']) > $setting->get('maximum_username_length')) toastr('error', 'Name must be less than ' . $setting->get('maximum_username_length') . ' characters');
-    if ($database->query("SELECT `display_name` FROM users WHERE display_name = :display_name", [':display_name' => $_POST['display_name']])->rowCount() > 0) toastr('error', 'Name already taken');
-    $info['display_name'] = $_POST['display_name'];
+    if ($database->query("SELECT `display_name` FROM users WHERE display_name = :display_name AND id != :id", [':display_name' => $_POST['display_name'], ':id' => $user->id()])->rowCount() > 0) toastr('error', 'Name already taken');
+    $info['display_name'] = secure($_POST['display_name']);
 } else $info['display_name'] = $user->name();
 
 if (isset($_POST['username']) && !empty(secure($_POST['username'])) && $_POST['username'] != $user->username()) {
-    if ($profile->allow_username_change()) toastr('error', 'Username change is not allowed');
+    if (!$profile->allow_username_change()) toastr('error', 'Username change is not allowed');
     if (strlen($_POST['username']) < $setting->get('minimum_username_length')) toastr('error', 'Username must be at least ' . $setting->get('minimum_username_length') . ' characters');
     if (strlen($_POST['username']) > $setting->get('maximum_username_length')) toastr('error', 'Username must be less than ' . $setting->get('maximum_username_length') . ' characters');
-    if ($database->query("SELECT `name` FROM users WHERE `name` = :username", [':username' => strtolower(secure($_POST['username']))])->rowCount() > 0) toastr('error', 'Username already taken');
-    $info['name'] = strtolower($_POST['username']);
+    if ($database->query("SELECT `name` FROM users WHERE `name` = :username AND id != :id", [':username' => strtolower(secure($_POST['username'])), ':id' => $user->id()])->rowCount() > 0) toastr('error', 'Username already taken');
+    $info['name'] = strtolower(secure($_POST['username']));
 } else $info['name'] = $user->username();
 
 if (isset($_POST['email']) && !empty(secure($_POST['email'])) && $_POST['email'] != $user->email()) {
-    if ($profile->allow_email_change()) toastr('error', 'Email change is not allowed');
+    if (!$profile->allow_email_change()) toastr('error', 'Email change is not allowed');
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) toastr('error', 'Invalid email address');
-    if ($database->query("SELECT `email` FROM users WHERE email = :email", [':email' => $_POST['email']])->rowCount() > 0) toastr('error', 'Email already taken');
+    if ($database->query("SELECT `email` FROM users WHERE email = :email AND id != :id", [':email' => $_POST['email'], ':id' => $user->id()])->rowCount() > 0) toastr('error', 'Email already taken');
     $info['email'] = $_POST['email'];
 } else $info['email'] = $user->email();
 
