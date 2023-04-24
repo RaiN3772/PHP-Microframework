03fbd38e332575cf4aad64a069e370414ec70d23
RaiN3772 <56936836+RaiN3772@users.noreply.github.com>
2023-04-24 04:27:36 +0300

    Update authentication.php

--- inc/init/authentication.php
+++ inc/init/authentication.php
@@ -1,8 +1,12 @@
 <?php
+// check if user is logged in and on the authentication or registration page
 if ($user->isLoggedIn() && in_array($page['current'], ['auth', 'register'])) {
+    // show info message that user is already logged in and redirect to home page
     toastr('info', 'You are already logged in', '/');
 }
 
+// check if user is not logged in and not on the authentication or registration page
 if (!$user->isLoggedIn() && !in_array($page['current'], ['auth', 'register'])) {
+    // redirect to the authentication page
     redirect('/auth');
 }
